db4098890637994f17c634a25a62ff51f082d2ee
Patrick Pichler <patrick@cast.ai>
2026-05-07 13:06:46 +0200

    feat: add kimchi-dev formula

---
+++ Formula/kimchi-dev.rb
@@ -0,0 +1,32 @@
+# typed: false
+# frozen_string_literal: true
+
+class KimchiDev < Formula
+  desc "A coding agent CLI powered by Cast AI"
+  homepage "https://github.com/castai/kimchi-dev"
+  version "0.0.26"
+  license "MIT"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_darwin_arm64.tar.gz"
+    sha256 "2ba502c3cb333957d7607d531c302a61e1c70b49eb7dbd9caf17cc589b94baef"
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_darwin_amd64.tar.gz"
+    sha256 "ebf65951b016e7d2e5245257b08407a012766600339ce39f7f1f291a8a0dc5e2"
+  elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_linux_arm64.tar.gz"
+    sha256 "f0777d7879728fcd00554c7593b6d2c345a007b9914c7fbd09aa400fefd68f53"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_linux_amd64.tar.gz"
+    sha256 "1bd59ca053d70761e35f8c6f5ff0aeedb1df5516b347899b097df6a9fa860d80"
+  end
+
+  def install
+    bin.install "bin/kimchi"
+    (share/"kimchi").install Dir["share/kimchi/*"]
+  end
+
+  test do
+    system "#{bin}/kimchi", "--version"
+  end
+end
