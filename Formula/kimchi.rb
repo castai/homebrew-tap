888a7045354f54a738f96848f6ae24693bee7aca
Žilvinas Bliudžius <zilvinas.b@cast.ai>
2026-05-15 14:53:32 +0300

    chore: rename formula to kimchi

--- Formula/kimchi.rb
+++ Formula/kimchi.rb
@@ -1,23 +1,23 @@
 # typed: false
 # frozen_string_literal: true
 
-class KimchiDev < Formula
+class Kimchi < Formula
   desc "A coding agent CLI powered by Cast AI"
-  homepage "https://github.com/castai/kimchi-dev"
+  homepage "https://github.com/castai/kimchi"
   version "0.0.46"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_darwin_arm64.tar.gz"
+    url "https://github.com/castai/kimchi/releases/download/v#{version}/kimchi_darwin_arm64.tar.gz"
     sha256 "549b840383d4bbe2853b239991b380dc67726f41d63ef460176fc392eb582c8f"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_darwin_amd64.tar.gz"
+    url "https://github.com/castai/kimchi/releases/download/v#{version}/kimchi_darwin_amd64.tar.gz"
     sha256 "00abcc98c171d3e9fd800423652c184efcd00e12ff2ddb661b8c1ff2aed6a981"
   elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_linux_arm64.tar.gz"
+    url "https://github.com/castai/kimchi/releases/download/v#{version}/kimchi_linux_arm64.tar.gz"
     sha256 "23b59e7e7458b6b2225e7bd744cc7463fc2840c67c732898b3fff7cf0cd75510"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/castai/kimchi-dev/releases/download/v#{version}/kimchi_linux_amd64.tar.gz"
+    url "https://github.com/castai/kimchi/releases/download/v#{version}/kimchi_linux_amd64.tar.gz"
     sha256 "f719ee9208fdc4236c2e16656b41054f74a8f470143aee3797629e332238d8a3"
   end
 
